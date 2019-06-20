cfb9849b48a6d7cd487075b149e50c72ea55acd1
marquesr1 <rafael.fmarques@outlook.com>
2019-06-20 19:52:16 -0300

    Updates

---
+++ precache-manifest.a17f78dbc3ae54149e17ba80605f071b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb9a9e0c83c566cb12276d8461e42712",
+    "url": "/react-personal-page/index.html"
+  },
+  {
+    "revision": "05bd7b80402ce1cee6e8",
+    "url": "/react-personal-page/static/js/2.27826fe3.chunk.js"
+  },
+  {
+    "revision": "8966f738b30bf394d326",
+    "url": "/react-personal-page/static/js/main.a9515318.chunk.js"
+  },
+  {
+    "revision": "02294f696ff6f72048b1",
+    "url": "/react-personal-page/static/js/runtime~main.b6326a13.js"
+  },
+  {
+    "revision": "8cc3327c52d0157ee02c8dd9f5dac320",
+    "url": "/react-personal-page/static/media/Curriculum.8cc3327c.pdf"
+  },
+  {
+    "revision": "3f6bce0076cf37e9612f64ef1746ddd6",
+    "url": "/react-personal-page/static/media/avatar.3f6bce00.png"
+  },
+  {
+    "revision": "efdba04681a3215e786c0e996e4bc780",
+    "url": "/react-personal-page/static/media/header-background.efdba046.jpg"
+  }
+]);
