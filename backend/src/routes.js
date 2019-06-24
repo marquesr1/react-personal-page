fa228544865fc9273c7b6d03f9bc31f2c3c3d584
marquesr1 <rafael.fmarques@outlook.com>
2019-06-24 11:37:55 -0300

    User CRUD

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,5 +4,9 @@ const routes = express.Router();
 const UsersController = require("./controllers/UsersController");
 
 routes.get("/users", UsersController.index);
+routes.get("/users/:id", UsersController.show);
+routes.post("/users", UsersController.store);
+routes.put("/users/:id", UsersController.update);
+routes.delete("/users/:id", UsersController.destroy);
 
 module.exports = routes;
