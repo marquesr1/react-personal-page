df5cf47069730bb633f01e9b295423f421e44b96
marquesr1 <rafael.fmarques@outlook.com>
2019-06-26 10:18:23 -0300

    CORS configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose
   .connect(
